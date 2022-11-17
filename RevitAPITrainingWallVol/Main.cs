2bca566dc081834a4579d3cad18d8dec3cf0a160
dom31 <dom31@LAPTOP-19N5M1CV>
2022-11-17 09:06:56 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingWallVol/Main.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingWallVol
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Face, "Выберите стены");
+            var elementList = new List<Double>();
+
+            foreach (var selectedElement in selectedElementRefList)
+{
+                Element element = doc.GetElement(selectedElement);
+                Parameter volumeParameter = element.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
+                if (volumeParameter.StorageType == StorageType.Double)
+                {
+                    double volumeValue = UnitUtils.ConvertFromInternalUnits(volumeParameter.AsDouble(), UnitTypeId.CubicMeters);
+                    elementList.Add(volumeValue);
+                         }
+                
+            }
+            
+            TaskDialog.Show("Объем выбранных стен", $"{elementList.Sum()}");
+            return Result.Succeeded;
+
+        }
+    }
+}
